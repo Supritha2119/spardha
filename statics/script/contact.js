b87602b4f862b610fa73bc49a7c519a0fff8cd17
Supritha2119 <supritha.rgowda2119@gmail.com>
2026-06-08 18:25:40 +0530

    Update phone numbers for contact branches

--- statics/script/contact.js
+++ statics/script/contact.js
@@ -11,19 +11,19 @@ const branches = {
   Kormangalla: {
     name: "Sri Spardha Academy - Kormangalla",
     address: "No 434, 1st floor, 17th Main Rd, KHB Colony, 5th Block, Koramangala, Bengaluru, Karnataka 560095",
-    phone: "+91 9980244099 / 9071389999 ",
+    phone: "+91 9980244099 ",
     map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.5849883398278!2d77.61607337484068!3d12.934373987377583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae144fb5e0a455%3A0x552f5f8f0910f744!2sSri%20Spardha%20Academy%20-%20Best%20Coaching%20for%20PSI%2CFDA%2CSDA%2CKAS%2CPDO%20and%20other%20Competitive%20exams%20in%20Bangalore.%20Library%2FStudy%20Centre!5e0!3m2!1sen!2sin!4v1768220266242!5m2!1sen!2sin"
   },
   Vajarahalli: {
     name: "Sri Spardha Academy - Vajarahalli",
     address: "587, 3rd Floor , 100 Feet Rd, Dwarakanagar, opposite Mariya Mart, Talaghattapura, Vajarahalli, Bengaluru, Karnataka 560062",
-    phone: "+91 9901731785 / 9901731758 ",
+    phone: "+91 9901731785 ",
     map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.5071742045766!2d77.5368341!3d12.875076000000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3fdf3cf7cb43%3A0x67c605e70d157907!2sSri%20Spardha%20Academy%20Vajarahalli!5e0!3m2!1sen!2sin!4v1768219842078!5m2!1sen!2sin"
   },
   Hoskote: {
     name: "Sri Spardha Academy - Hoskote",
     address: "65, 3rd Cross Rd, near Srinivasa Hospital, T. G. Extension, Kammavari Pete, Hoskote, Bengaluru, Karnataka 562114",
-    phone: "+91 7760508507 / 9353000187 ",
+    phone: "+91 9353000187 ",
     map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.393523582691!2d77.78272107484328!3d13.074227987250621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae05a7996810d7%3A0x792d701d42b3b62e!2sSri%20Spardha%20Academy%20Hosakote%20-%20Coaching%20for%20PSI%2CFDA%2CSDA%2CKAS%2CPDO%2CBanking%20and%20other%20Competitive%20exams%20in%20Hoskote.!5e0!3m2!1sen!2sin!4v1768220368414!5m2!1sen!2sin"
   }
 };
